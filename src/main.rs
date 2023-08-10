9f6948a5fe346decc047731edd40a9b63dac8502
zakissimo <zakaria.habri@gmail.com>
2023-08-10 22:44:45 +0200

    Implementing caching logic

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
+use chrono::{DateTime, Local, NaiveTime};
 use reqwest::blocking::Client;
-use chrono::{NaiveTime, Local};
 use select::document::Document;
 use select::predicate::Name;
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 use serde_json;
 use std::fmt;
-// use chrono::DateTime;
+use std::fs;
+use std::path::Path;
 
-fn fetch() -> Result<reqwest::blocking::Response, reqwest::Error> {
-    let url = "https://mawaqit.net/fr/m-angouleme";
-    // let url = "https://mawaqit.net/fr/mosquee-dagen";
-
-    let client = Client::new();
-    return client.get(url).send();
-}
-
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 struct Data {
     times: [String; 5],
     name: String,
@@ -49,6 +42,14 @@ impl fmt::Display for Data {
     }
 }
 
+fn fetch() -> Result<reqwest::blocking::Response, reqwest::Error> {
+    let url = "https://mawaqit.net/fr/m-angouleme";
+    // let url = "https://mawaqit.net/fr/mosquee-dagen";
+
+    let client = Client::new();
+    return client.get(url).send();
+}
+
 fn parse(doc: Document) -> Result<Data, String> {
     for element in doc.find(Name("script")) {
         if element.inner_html().contains("var confData") {
@@ -60,7 +61,7 @@ fn parse(doc: Document) -> Result<Data, String> {
                     .trim_start_matches("var confData = ")
                     .trim_end_matches(";");
                 if json_string.starts_with("{") {
-                    let data : Result<Data, serde_json::Error> = serde_json::from_str(json_string);
+                    let data: Result<Data, serde_json::Error> = serde_json::from_str(json_string);
                     match data {
                         Ok(data) => return Ok(data),
                         Err(_) => return Err("Couldn't parse html response!".to_string()),
@@ -72,6 +73,15 @@ fn parse(doc: Document) -> Result<Data, String> {
     Err("Couldn't parse html response!".to_string())
 }
 
+fn fetch_and_parse() -> Option<Data> {
+    let r = fetch().ok()?;
+    let body = r.text().ok()?;
+    let doc = Document::from(body.as_str());
+    let data = parse(doc).ok()?;
+
+    Some(data)
+}
+
 fn get_remaining_time(data: Data) -> String {
     let now = Local::now().time();
 
@@ -79,9 +89,8 @@ fn get_remaining_time(data: Data) -> String {
         .times
         .into_iter()
         .filter_map(|time| NaiveTime::parse_from_str(&time, "%H:%M").ok()) // Filter out invalid times
-        .filter(|&time| time > now) // Filter times that are greater than the current time
+        .filter(|&time| time > now)
         .fold(None, |closest_time, time| {
-            // Calculate the closest time to the current time
             match closest_time {
                 Some(prev_time) => {
                     if time < prev_time {
@@ -97,30 +106,88 @@ fn get_remaining_time(data: Data) -> String {
     match remaining_time {
         Some(time) => {
             let duration = time.signed_duration_since(now);
-            format!("{}:{:02}", duration.num_hours(), duration.num_minutes() % 60)
+            format!(
+                "{}:{:02}",
+                duration.num_hours(),
+                duration.num_minutes() % 60
+            )
         }
         None => "??:??".to_string(),
     }
 }
 
-fn main() {
-    let r = fetch();
-    match r {
-        Ok(r) => match r.text() {
-            Ok(body) => {
-                let doc = Document::from(body.as_str());
-                let data = parse(doc);
-                match data {
-                    Ok(data) =>  {
-                        let s = get_remaining_time(data);
-                        println!("{s}");
-
-                    },
-                    Err(_) => eprintln!("Error while parsing the document."),
-                }
-            },
-            Err(_) => eprintln!("Error while decoding the response to text."),
-        },
-        Err(_) => eprintln!("Error while sending request."),
-    };
+fn file_exists(file_path: &str) -> bool {
+    Path::new(file_path).exists()
 }
+
+fn get_last_modified(file_path: &str) -> Option<DateTime<Local>> {
+    fs::metadata(file_path)
+        .ok()
+        .and_then(|metadata| metadata.modified().ok().map(|modified| modified.into()))
+}
+
+fn build_file() {
+    let file_path = "/tmp/Time4Salat.log";
+    let data = fetch_and_parse();
+
+    if let Some(data) = data {
+        let content = serde_json::to_string(&data).unwrap();
+        if let Err(err) = fs::write(file_path, content) {
+            eprintln!("Error writing to file: {}", err);
+        }
+    } else {
+        eprintln!("Error: No data available.");
+    }
+}
+
+fn get_data() -> Option<Data> {
+    let file_path = "/tmp/Time4Salat.log";
+    if !file_exists(file_path) {
+        build_file();
+    } else {
+        let last_modified = get_last_modified(file_path);
+        match last_modified {
+            Some(last_modified) => {
+                let now = Local::now().format("%d/%m/%y").to_string();
+                let last_modified = last_modified.format("%d/%m/%y").to_string();
+                if now != last_modified {
+                    build_file();
+                }
+            }
+            None => eprintln!("Couldn't get last modified date of file."),
+        }
+    }
+    let content = fs::read_to_string(file_path).ok()?;
+    match serde_json::from_str(&content) {
+    Ok(data) => Some(data),
+    Err(err) => {
+        eprintln!("Error parsing JSON: {}", err);
+        None
+    }
+}
+}
+
+fn main() {
+    println!("{}", get_remaining_time(get_data().unwrap()));
+}
+
+// fn main() {
+//     let file_path = "/tmp/Time4Salat.log";
+//
+//     if should_update_file(file_path) {
+//         if let Some(data) = fetch_and_parse_data() {
+//             if let Err(err) = update_data_file(&data) {
+//                 eprintln!("Error updating file: {}", err);
+//             }
+//         } else {
+//             eprintln!("Error: Failed to fetch and parse data.");
+//         }
+//     }
+//
+//     if let Some(data) = get_data_from_file(file_path) {
+//         let remaining_time = get_remaining_time(data);
+//         println!("{}", remaining_time);
+//     } else {
+//         eprintln!("Error: Failed to get data from file.");
+//     }
+// }
